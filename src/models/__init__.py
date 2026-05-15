d2a6e384a5f6aec812a2e4c9ab812b5ed4c1e2b1
Vaccummer <2316374406@qq.com>
2026-05-15 15:55:19 +0800

    feat: register ScheduleAttachment in model factory and exports

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,11 +4,20 @@ from __future__ import annotations
 
 from models.attachment import TodoAttachment
 from models.schedule import Schedule
+from models.schedule_attachment import ScheduleAttachment
 from models.setting import Setting
 from models.todo import Todo
 from models.user import User
 
-__all__ = ["Schedule", "Setting", "Todo", "TodoAttachment", "User", "register_models"]
+__all__ = [
+    "Schedule",
+    "ScheduleAttachment",
+    "Setting",
+    "Todo",
+    "TodoAttachment",
+    "User",
+    "register_models",
+]
 
 
 def register_models() -> None:
